2b956029b39face5baa8ffaeb58b149cd978c519
Vinoth Kanna <98391866+vinothkanna273@users.noreply.github.com>
2023-07-09 20:55:06 +0530

    Data retrieve Firebase to toolsfragment
    Data retrieve from Firebase to Toolsfragment

--- app/src/main/java/com/example/sstrial/ToolsFragment.java
+++ app/src/main/java/com/example/sstrial/ToolsFragment.java
@@ -2,14 +2,28 @@ package com.example.sstrial;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ToolsFragment extends Fragment {
@@ -34,6 +48,7 @@ public class ToolsFragment extends Fragment {
     FirebaseAuth auth;
     FirebaseUser user;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -41,8 +56,91 @@ public class ToolsFragment extends Fragment {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
         String Uid = auth.getUid();
+        LinearLayout linearLayout = view.findViewById(R.id.recordBox);
 
+
+        DatabaseReference dataRef = FirebaseDatabase.getInstance().getReference().child("user").child(Uid).child("storage");
+        dataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int i = 0;
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String dateTime = dataSnapshot.child("dateTime").getValue(String.class);
+                    String[] array = dateTime.split(" ");
+                    String date = array[0];
+                    String time = array[1];
+                    String imageUrl = dataSnapshot.child("imageUrl").getValue(String.class);
+                    String result = dataSnapshot.child("result").getValue(String.class);
+
+                    LinearLayout linearLayoutItem = createLinearLayout();
+                    linearLayoutItem.setOrientation(LinearLayout.VERTICAL);
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                    );
+                    linearLayoutItem.setPadding(40,20,40,40);
+                    layoutParams.setMargins(50, 20, 50, 20);
+                    linearLayoutItem.setLayoutParams(layoutParams);
+                    linearLayoutItem.setBackground(getContext().getDrawable(R.drawable.custom_edittext));
+
+                    int desiredWidth = 540;
+                    int desiredHeight = 540;
+                    ImageView uploadImage = new ImageView(getContext());
+                    LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(desiredWidth, desiredHeight);
+                    layoutParams1.gravity = Gravity.CENTER;
+                    uploadImage.setLayoutParams(layoutParams1);
+
+                    if (imageUrl != null && !imageUrl.isEmpty()) {
+                        Glide.with(requireContext())
+                                .load(imageUrl)
+                                .placeholder(R.drawable.medicine) // Default image resource
+                                .into(uploadImage);
+                    } else {
+                        uploadImage.setImageResource(R.drawable.medicine); // Use default image
+                    }
+
+                    TextView dateTextView = createTextView(date, 22);
+                    TextView timeTextView = createTextView(time, 22);
+                    TextView resultTextView = createTextView(result, 22);
+
+                    linearLayoutItem.addView(dateTextView);
+                    linearLayoutItem.addView(timeTextView);
+                    linearLayoutItem.addView(resultTextView);
+                    linearLayoutItem.addView(uploadImage);
+
+                    // Add LinearLayoutItem to the parent LinearLayout
+                    linearLayout.addView(linearLayoutItem);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return view;
+    }
+
+
+    private TextView createTextView(String text, int textSize) {
+        return createTextView(text, textSize, false);
+    }
+
+    private TextView createTextView(String text, int textSize, boolean bold) {
+        TextView textView = new TextView(getContext());
+        textView.setText(text);
+        textView.setTextSize(textSize);
+        textView.setTextColor(getResources().getColor(R.color.green2));
+        if (bold) {
+            textView.setTypeface(textView.getTypeface(), android.graphics.Typeface.BOLD);
+        }
+        return textView;
+    }
+
+    private LinearLayout createLinearLayout() {
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        // Set any additional properties for the LinearLayout
+        return linearLayout;
     }
 }
